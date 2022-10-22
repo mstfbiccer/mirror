0c9d708582c8e0d239fc49b7f248aa7b0f466a9c
Mustafa BİÇER <mstfbiccer@gmail.com>
2022-10-22 10:03:35 +0300

    added comment lines

--- index.js
+++ index.js
@@ -2,18 +2,22 @@ const { app, BrowserWindow, desktopCapturer, screen } = require('electron')
 const path = require('path')
 let win;
 
+// Create a new BrowserWindow when `app` is ready.
 const createWindow = () => {
+  // get the primary display resolution.
   let display = screen.getPrimaryDisplay();
   let width = display.bounds.width;
   let height = display.bounds.height;
 
-
+  // Create the browser window.
   win = new BrowserWindow({
     width: 500,
     height: 350,
+    // preaload script to access nodejs api in renderer process.
     webPreferences: {
       preload: path.join(__dirname, './preload.js'),
     },
+    // window position set to bottom right corner. 
     x: width - 500,
     y: height - 400,
     movable:true,
@@ -21,20 +25,26 @@ const createWindow = () => {
     alwaysOnTop: true
   })
 
+  // Load index.html into the new BrowserWindow.
   win.loadFile('index.html')
   
 }
 app.whenReady().then(() => {
+  // Create a new window after electron is ready.
   createWindow();
   
 })
+
+// get the sources and send it to renderer process.
 desktopCapturer.getSources({ types: ['window','screen', ]}).then(async sources => {
   for (const source of sources) {
     if(source.name.indexOf("Google Meet") >= 0 || source.name.indexOf('Meet –') >= 0) {
+      // send the source id to renderer process.
       win.webContents.send('SET_SOURCE', source.id)
       return;
     }
   }
+  // if no source found, send the null value to renderer process.
   win.webContents.send('SET_SOURCE', null)
 }).catch((err) => {
   console.log(err);
--- preload.js
+++ preload.js
@@ -1,9 +1,13 @@
 // In the preload script.
 const { ipcRenderer } = require('electron')
 
+// listen for the source id from main process.
 ipcRenderer.on('SET_SOURCE', async (event, sourceId) => {
+  // if source id isn't null, create a stream from the source id.
+  // else, then no source found.
   if(sourceId) {
     try {
+      // create a stream from the source id.
       const stream = await navigator.mediaDevices.getUserMedia({
         audio: false,
         video: {
@@ -22,6 +26,7 @@ ipcRenderer.on('SET_SOURCE', async (event, sourceId) => {
       handleError(e)
     }
   }else {
+    // no source found.
     const container = document.querySelector('.container');
     container.innerHTML = '<p>Any Google Meet tab is not found. Please open Google Meet tab and try again.</p>';
   }
@@ -29,11 +34,13 @@ ipcRenderer.on('SET_SOURCE', async (event, sourceId) => {
 })
 
 function handleStream (stream) {
+  // stream data send to video element on the DOM.
   const video = document.getElementById('recordData')
   video.srcObject = stream
-  video.onloadedmetadata = (e) => video.play()
+  video.onloadedmetadata = () => video.play()
 }
 
+// error handling function.
 function handleError (e) {
   console.log(e)
 }
